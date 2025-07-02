6876915b97cacbe62415bc365cf6692e558d152b
andriy29k <andriykorotchuk345@gmail.com>
2025-07-02 22:39:34 +0300

    Test CD

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.google_credentials_file)
+  credentials = var.google_credentials_file
   project     = var.project_id
   region      = var.region
   zone        = var.zone
